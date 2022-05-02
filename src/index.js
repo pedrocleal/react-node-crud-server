cd6293e72585b5ecc852f9bbad124921a0d4d4ad
pedrocleal <pedroxleal10@gmail.com>
2022-05-01 23:32:40 -0300

    Add cors to server

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const routes = require('./routes');
 
 const server = express();
-
 server.use(express.json());
+server.use(cors());
 server.use(routes);
 
 server.listen(3030, () => {
